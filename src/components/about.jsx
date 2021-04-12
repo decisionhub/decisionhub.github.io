1e12860270ffd1e1dd2825c7ec80ef41ffda7d54
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2021-04-12 01:22:22 -0400

    Update about section

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -11,6 +11,7 @@ export const About = (props) => {
             <div className='about-text'>
               <h2>About Us</h2>
               <p>{props.data ? props.data.paragraph : 'loading...'}</p>
+              <p>{props.data ? props.data.paragraph2 : 'loading...'}</p>
               <h3>Why Choose Us?</h3>
               <div className='list-style'>
                 <div className='col-lg-6 col-sm-6 col-xs-12'>
