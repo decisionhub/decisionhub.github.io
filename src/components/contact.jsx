dde7ff298375702876d21d6b09b34a3a7d03352c
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2021-05-23 12:37:01 -0400

    Setup linters and circle CI

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,32 +1,30 @@
 export const Contact = (props) => {
   return (
     <div>
-      <div id='footer'>
-        <div className='social'>
+      <div id="footer">
+        <div className="social">
           <ul>
             <li>
-              <a href={props.data ? props.data.email : '/'}>
-                <i className='fa fa-envelope'></i>
+              <a href={props.data ? props.data.email : "/"}>
+                <i className="fa fa-envelope"></i>
               </a>
             </li>
             <li>
-              <a href={props.data ? props.data.twitter : '/'}>
-                <i className='fa fa-twitter'></i>
+              <a href={props.data ? props.data.twitter : "/"}>
+                <i className="fa fa-twitter"></i>
               </a>
             </li>
             <li>
-              <a href={props.data ? props.data.linkedin : '/'}>
-                <i className='fa fa-linkedin'></i>
+              <a href={props.data ? props.data.linkedin : "/"}>
+                <i className="fa fa-linkedin"></i>
               </a>
             </li>
           </ul>
         </div>
-        <div className='container text-center'>
-          <p>
-            &copy; 2021 DecisionHub
-          </p>
+        <div className="container text-center">
+          <p>&copy; 2021 DecisionHub</p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
--- src/components/team.jsx
+++ src/components/team.jsx
@@ -1,46 +1,47 @@
 export const Team = (props) => {
   return (
-    <div id='team' className='text-center'>
-      <div className='container'>
-        <div className='col-md-8 col-md-offset-2 section-title'>
+    <div id="team" className="text-center">
+      <div className="container">
+        <div className="col-md-8 col-md-offset-2 section-title">
           <h2>Meet the Team</h2>
           <p>
-            Interested to learn more about the team behind DecisionHub? Find out here.
+            Interested to learn more about the team behind DecisionHub? Find out
+            here.
           </p>
         </div>
-        <div className='container' id='row' style={{width: '60%'}}>
+        <div className="container" id="row" style={{ width: "60%" }}>
           {props.execTeam
             ? props.execTeam.map((d, i) => (
-                <div key={`${d.name}-${i}`} className='col-sm-6 team'>
-                  <div className='thumbnail'>
-                    {' '}
-                    <img src={d.img} alt='Team Member' className='team-img' />
-                    <div className='caption'>
+                <div key={`${d.name}-${i}`} className="col-sm-6 team">
+                  <div className="thumbnail">
+                    {" "}
+                    <img src={d.img} alt="Team Member" className="team-img" />
+                    <div className="caption">
                       <h4>{d.name}</h4>
                       <p>{d.job}</p>
                     </div>
                   </div>
                 </div>
               ))
-            : 'loading'}
+            : "loading"}
         </div>
-        <div className='container' id='row' style={{width: '90%'}}>
+        <div className="container" id="row" style={{ width: "90%" }}>
           {props.team
             ? props.team.map((d, i) => (
-                <div key={`${d.name}-${i}`} className='col-sm-4 team'>
-                  <div className='thumbnail'>
-                    {' '}
-                    <img src={d.img} alt='Team Member' className='team-img' />
-                    <div className='caption'>
+                <div key={`${d.name}-${i}`} className="col-sm-4 team">
+                  <div className="thumbnail">
+                    {" "}
+                    <img src={d.img} alt="Team Member" className="team-img" />
+                    <div className="caption">
                       <h4>{d.name}</h4>
                       <p>{d.job}</p>
                     </div>
                   </div>
                 </div>
               ))
-            : 'loading'}
+            : "loading"}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
