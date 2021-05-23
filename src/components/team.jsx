65e0b3096ac57d7fc6e6c6ed7bc3dba747496f54
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2021-05-23 13:36:28 -0400

    Merge conflicts from lint changes on master

--- src/components/team.jsx
+++ src/components/team.jsx
@@ -9,30 +9,30 @@ export const Team = (props) => {
             here.
           </p>
         </div>
-        <div className='container' id='row' style={{width: '60%'}}>
+        <div className="container" id="row" style={{ width: "60%" }}>
           {props.execTeam
             ? props.execTeam.map((d, i) => (
-                <div key={`${d.name}-${i}`} className='col-sm-6 team'>
-                  <div className='thumbnail'>
-                    {' '}
-                    <img src={d.img} alt='Team Member' className='team-img' />
-                    <div className='caption'>
+                <div key={`${d.name}-${i}`} className="col-sm-6 team">
+                  <div className="thumbnail">
+                    {" "}
+                    <img src={d.img} alt="Team Member" className="team-img" />
+                    <div className="caption">
                       <h4>{d.name}</h4>
                       <p>{d.job}</p>
                     </div>
                   </div>
                 </div>
               ))
-            : 'loading'}
+            : "loading"}
         </div>
-        <div className='container' id='row' style={{width: '90%'}}>
+        <div className="container" id="row" style={{ width: "90%" }}>
           {props.team
             ? props.team.map((d, i) => (
-                <div key={`${d.name}-${i}`} className='col-sm-4 team'>
-                  <div className='thumbnail'>
-                    {' '}
-                    <img src={d.img} alt='Team Member' className='team-img' />
-                    <div className='caption'>
+                <div key={`${d.name}-${i}`} className="col-sm-4 team">
+                  <div className="thumbnail">
+                    {" "}
+                    <img src={d.img} alt="Team Member" className="team-img" />
+                    <div className="caption">
                       <h4>{d.name}</h4>
                       <p>{d.job}</p>
                     </div>
