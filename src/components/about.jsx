992c3495eaa5571cbc899be8b3c0c9b72ce7a4a0
Derek <dmaxin@uwaterloo.ca>
2021-05-24 13:59:38 -0400

    Added a meta description

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,6 +1,6 @@
 export const About = (props) => {
   return (
-    <div id="aboot">
+    <div id="about">
       <div className="container">
         <div className="row">
           <div className="col-xs-12 col-md-6">
