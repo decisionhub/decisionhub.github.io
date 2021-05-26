ee268b694fafccdfbefe71feb3d55f7138fd5dcf
derekmaxin <83664901+derekmaxin@users.noreply.github.com>
2021-05-26 09:35:34 -0400

    Derek/fix image stretch and add alt field (#8)
    * image changes
    
    * image changes
    
    * added object-fit: cover and text to the alt field

--- src/components/testimonials.jsx
+++ src/components/testimonials.jsx
@@ -12,7 +12,7 @@ export const Testimonials = (props) => {
                   <div className="testimonial">
                     <div className="testimonial-image">
                       {" "}
-                      <img src={d.img} alt="" />{" "}
+                      <img src={d.img} alt="Testimonial headshot" />{" "}
                     </div>
                     <div className="testimonial-content">
                       <p>
