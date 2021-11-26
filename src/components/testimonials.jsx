5ee49d25eaf8a3a721574109a82848fe08d11acf
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2021-11-25 22:53:16 -0500

    Add new testimonial and redesign section

--- src/components/testimonials.jsx
+++ src/components/testimonials.jsx
@@ -1,28 +1,30 @@
 export const Testimonials = (props) => {
   return (
     <div id="testimonials">
-      <div className="container">
-        <div className="section-title text-center">
-          <h2>What our clients say</h2>
-        </div>
-        <div className="row">
+      <div className="section-title text-center">
+        <h2>What our clients say</h2>
+      </div>
+      <div
+        className="container"
+        style={{ display: "flex", textAlign: "center" }}
+      >
+        <div className="row" style={{ margin: "auto" }}>
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className="col-md-6">
-                  <div className="testimonial">
-                    <div className="testimonial-image">
-                      {" "}
-                      <img src={d.img} alt="Testimonial headshot" />{" "}
-                    </div>
-                    <div className="testimonial-content">
-                      <p>
-                        {'"'}
-                        {d.text}
-                        {'"'}
-                      </p>
-                      <div className="testimonial-meta"> - {d.name} </div>
-                    </div>
-                  </div>
+                <div
+                  key={`${d.name}-${i}`}
+                  className="col-md-4"
+                  style={{ height: 450, maxWidth: 400, margin: "auto" }}
+                >
+                  <img
+                    src={d.img}
+                    className="img-circle"
+                    alt="Customer Feedback"
+                    style={{ width: 200, height: 200 }}
+                  />
+                  <h3>{d.name}</h3>
+                  <h5>{d.role}</h5>
+                  <p>{d.text}</p>
                 </div>
               ))
             : "loading"}
