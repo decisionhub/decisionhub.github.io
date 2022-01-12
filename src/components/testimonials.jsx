2531a36ab6c2eed43859ac410cbc4d1b586588d7
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2022-01-12 12:23:00 -0500

    More css changes

--- src/components/testimonials.jsx
+++ src/components/testimonials.jsx
@@ -2,7 +2,7 @@ import Carousel from "react-material-ui-carousel";
 
 export const Testimonials = (props) => {
   return (
-    <div>
+    <div id="testimonials-continued">
       <div className="section-title text-center">
         <h2>What our clients say</h2>
       </div>
