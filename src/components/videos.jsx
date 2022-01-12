7d992f18f8b7d1a3cb5fe1957b13c9bfc676e3f8
Ahmed Hamodi <ahmed.r.hamodi@gmail.com>
2022-01-12 12:32:15 -0500

    Add new testimonial videos (#41)
    * Add Taryn testimonial video
    
    * Add second video iframe (same link for now)
    
    * Update src/data/data.json
    
    * Fix where navbar goes on testimonial click
    
    * Add rel to external links
    
    * Update src/components/contact.jsx
    
    * Remove testimonial for now
    
    * Remove plural
    
    * More css changes

---
+++ src/components/videos.jsx
@@ -0,0 +1,29 @@
+export const Videos = (props) => {
+  return (
+    <div id="testimonials" className="text-center">
+      <div className="container">
+        <div className="col-md-8 col-md-offset-2 section-title">
+          <h2>Customer Testimonials</h2>
+          <p>
+            Hear what our clients have to say about DecisionHub in this video!
+          </p>
+        </div>
+        {props.data
+          ? props.data.map((video, i) => (
+              <iframe
+                key={i}
+                style={{ margin: 20 }}
+                width="464"
+                height="261"
+                src={video.link}
+                title="YouTube video player, customer testimonial"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              />
+            ))
+          : "loading"}
+      </div>
+    </div>
+  );
+};
