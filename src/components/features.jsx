6ec2e638ba47f7ec6c444831bdb0d0beede27ccd
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2022-01-23 14:51:15 -0500

    [WIP] Refactor website into subpages

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -1,38 +1,50 @@
 export const Features = (props) => {
   return (
-    <div id="features" className="text-center">
-      <div className="container">
-        <div className="col-md-8 col-md-offset-2 section-title">
-          <h2>Features</h2>
-        </div>
-        <div className="row">
-          {props.data
-            ? props.data.map((d, i) => (
-                <div
-                  key={`${d.title}-${i}`}
-                  className="col-xs-12 col-sm-12 col-md-6 col-lg-3"
-                  style={{ marginBottom: 20 }}
-                >
-                  <i className={d.icon}></i>
-                  <h3>{d.title}</h3>
-                  <p>{d.text}</p>
-                </div>
-              ))
-            : "Loading..."}
-        </div>
-        <div className="col-md-8 col-md-offset-2 section-title">
-          <h2 style={{ marginTop: 60 }}>Demo</h2>
-          <iframe
-            width="560"
-            height="315"
-            src="https://www.youtube.com/embed/ivCk8-kwKyw"
-            title="YouTube video player, DecisionHub demo"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          />
+    <>
+      <div id="features" className="text-center">
+        <div className="container">
+          <div className="col-md-8 col-md-offset-2 section-title">
+            <h2>Features</h2>
+          </div>
+          <div className="row">
+            {props.data
+              ? props.data.map((d, i) => (
+                  <div
+                    key={`${d.title}-${i}`}
+                    className="col-xs-12 col-sm-12 col-md-6 col-lg-3"
+                    style={{ marginBottom: 20 }}
+                  >
+                    <i className={d.icon}></i>
+                    <h3>{d.title}</h3>
+                    <p>{d.text}</p>
+                  </div>
+                ))
+              : "Loading..."}
+          </div>
         </div>
       </div>
-    </div>
+      <div id="features-continued" className="text-center">
+        <div className="container">
+          <div className="col-md-8 col-md-offset-2 section-title">
+            <div className="section-title">
+              <h2>Product Demo</h2>
+              <p>
+                5-minute demo video showing all the key aspects of
+                DecisionHub&apos;s platform.
+              </p>
+            </div>
+            <iframe
+              width="672"
+              height="378"
+              src="https://www.youtube.com/embed/ivCk8-kwKyw"
+              title="YouTube video player, DecisionHub demo"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
