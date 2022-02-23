8d3730a03ef619ff10c060d42d58df2f0c436baa
Ahmed Hamodi <ahmed.r.hamodi@gmail.com>
2022-02-23 09:27:21 -0500

    Update pricing page (#90)

--- src/components/pricing.jsx
+++ src/components/pricing.jsx
@@ -3,73 +3,97 @@ export const Pricing = () => {
     <div id="pricing" className="text-center">
       <div className="container">
         <div className="section-title">
-          <h2>Our Pricing</h2>
+          <h2>Save Time Making Decisions</h2>
           <p>
-            We offer 3 plans with various access levels on DecisionHub. Each
-            plan has a <b>1 month free trial</b> option!
+            We believe the most valuable asset for you and your organization is
+            your time. Why waste time in meetings when you can use a software
+            solution that has proven to save organizations like yours hundreds
+            of hours? Find out more about our plans below.
           </p>
         </div>
         <div className="container-fluid">
           <div className="container" style={{ width: "100%", maxWidth: 1000 }}>
             <div className="row">
-              <div className="col-sm-4" style={{ paddingBottom: 50 }}>
+              <div className="col-md-4" style={{ paddingBottom: 50 }}>
                 <div className="card text-center">
                   <div className="title">
                     <i className="fa fa-paper-plane" aria-hidden="true"></i>
                     <h2>Basic</h2>
                   </div>
-                  <div className="price">
-                    <h4>
+                  <div
+                    className="price"
+                    style={{ height: 200, display: "flex" }}
+                  >
+                    <h4 style={{ margin: "auto" }}>
                       <sup>$</sup>300
                       <span style={{ fontSize: 16 }}>/mo</span>
+                      <p style={{ color: "#fff", marginTop: 10 }}>
+                        + $50 / reviewer
+                      </p>
+                      <p style={{ color: "#fff" }}>+ $1 / application</p>
+                      <p style={{ color: "#fff" }}>+ $300 / form</p>
                     </h4>
                   </div>
-                  <div className="option">
+                  <div className="option" style={{ minHeight: 320 }}>
                     <ul>
                       <li>
                         <i className="fa fa-check" aria-hidden="true"></i> 1
-                        Program
+                        Form
                       </li>
                       <li>
                         <i className="fa fa-check" aria-hidden="true"></i> 20
                         Applications
                       </li>
                       <li>
-                        <i className="fa fa-check" aria-hidden="true"></i> 1
-                        Reviewer
+                        <i className="fa fa-check" aria-hidden="true"></i> 2
+                        Reviewers
                       </li>
                       <li>
-                        <i className="fa fa-times" aria-hidden="true"></i> No
-                        Logo Branding
+                        <i className="fa fa-check" aria-hidden="true"></i>{" "}
+                        Unlimited Questions
+                      </li>
+                      <li>
+                        <i className="fa fa-check" aria-hidden="true"></i>{" "}
+                        Limited Data Exports
                       </li>
                     </ul>
                   </div>
-                  <a href="https://app.decisionhub.org/signup">
-                    Start Free Trial
+                  <a
+                    href="https://calendly.com/decisionhub"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Book a Demo
                   </a>
                 </div>
               </div>
 
-              <div className="col-sm-4" style={{ paddingBottom: 50 }}>
+              <div className="col-md-4" style={{ paddingBottom: 50 }}>
                 <div className="card text-center">
                   <div className="title">
                     <i className="fa fa-plane" aria-hidden="true"></i>
                     <h2>Standard</h2>
                   </div>
-                  <div className="price">
-                    <h4>
+                  <div
+                    className="price"
+                    style={{ height: 200, display: "flex" }}
+                  >
+                    <h4 style={{ margin: "auto" }}>
                       <sup>$</sup>600
                       <span style={{ fontSize: 16 }}>/mo</span>
+                      <p style={{ color: "#fff" }}>+ $25 / reviewer</p>
+                      <p style={{ color: "#fff" }}>+ $0.5 / application</p>
+                      <p style={{ color: "#fff" }}>+ $150 / form</p>
                     </h4>
                   </div>
-                  <div className="option">
+                  <div className="option" style={{ minHeight: 320 }}>
                     <ul>
                       <li>
                         <i className="fa fa-check" aria-hidden="true"></i> 2
-                        Programs
+                        Forms
                       </li>
                       <li>
-                        <i className="fa fa-check" aria-hidden="true"></i> 100
+                        <i className="fa fa-check" aria-hidden="true"></i> 50
                         Applications
                       </li>
                       <li>
@@ -77,51 +101,94 @@ export const Pricing = () => {
                         Reviewers
                       </li>
                       <li>
+                        <i className="fa fa-check" aria-hidden="true"></i>{" "}
+                        Unlimited Questions
+                      </li>
+                      <li>
+                        <i className="fa fa-check" aria-hidden="true"></i> All
+                        Data Exports
+                      </li>
+                      <li>
                         <i className="fa fa-check" aria-hidden="true"></i> Logo
                         Branding
                       </li>
+                      <li>
+                        <i className="fa fa-check" aria-hidden="true"></i> Email
+                        Support
+                      </li>
                     </ul>
                   </div>
-                  <a href="https://app.decisionhub.org/signup">
-                    Start Free Trial
+                  <a
+                    href="https://calendly.com/decisionhub"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Book a Demo
                   </a>
                 </div>
               </div>
 
-              <div className="col-sm-4" style={{ paddingBottom: 50 }}>
+              <div className="col-md-4" style={{ paddingBottom: 50 }}>
                 <div className="card text-center">
                   <div className="title">
                     <i className="fa fa-rocket" aria-hidden="true"></i>
-                    <h2>Premium</h2>
+                    <h2>Enterprise</h2>
                   </div>
-                  <div className="price">
-                    <h4>
-                      <sup>$</sup>1200
-                      <span style={{ fontSize: 16 }}>/mo</span>
+                  <div
+                    className="price"
+                    style={{ height: 200, display: "flex" }}
+                  >
+                    <h4 style={{ margin: "auto" }}>
+                      Custom
+                      <h5 style={{ fontSize: 25 }}>Contact Us</h5>
                     </h4>
                   </div>
-                  <div className="option">
+                  <div className="option" style={{ minHeight: 320 }}>
                     <ul>
                       <li>
-                        <i className="fa fa-check" aria-hidden="true"></i> 5
-                        Programs
+                        <i className="fa fa-check" aria-hidden="true"></i>{" "}
+                        Starting from 5 Forms
                       </li>
                       <li>
                         <i className="fa fa-check" aria-hidden="true"></i>{" "}
-                        Unlimited Applications
+                        Starting from 100 Applications
                       </li>
                       <li>
                         <i className="fa fa-check" aria-hidden="true"></i>{" "}
-                        Unlimied Reviewers
+                        Starting from 20 Reviewers
                       </li>
                       <li>
-                        <i className="fa fa-check" aria-hidden="true"></i> Logo
-                        Branding
+                        <i className="fa fa-check" aria-hidden="true"></i>{" "}
+                        Unlimited Questions
+                      </li>
+                      <li>
+                        <i className="fa fa-check" aria-hidden="true"></i> All
+                        Data Exports
+                      </li>
+                      <li>
+                        <i className="fa fa-check" aria-hidden="true"></i>{" "}
+                        Custom Logo Branding, Colours, and Form URL
+                      </li>
+                      <li>
+                        <i className="fa fa-check" aria-hidden="true"></i>{" "}
+                        Priority Support
+                      </li>
+                      <li>
+                        <i className="fa fa-check" aria-hidden="true"></i>{" "}
+                        Custom Features
+                      </li>
+                      <li>
+                        <i className="fa fa-check" aria-hidden="true"></i>{" "}
+                        Custom Integrations
                       </li>
                     </ul>
                   </div>
-                  <a href="https://app.decisionhub.org/signup">
-                    Start Free Trial
+                  <a
+                    href="https://calendly.com/decisionhub"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Book a Demo
                   </a>
                 </div>
               </div>
@@ -129,6 +196,9 @@ export const Pricing = () => {
           </div>
         </div>
         <p style={{ paddingTop: 20 }}>
+          Each plan has a <b>1 month free trial</b> option.
+        </p>
+        <p>
           If you are a non-profit organization, a social enterprise, or need a{" "}
           <b>custom</b> plan, please{" "}
           <a href="mailto:support@decisionhub.org">
