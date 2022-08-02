865d80256e2635365fbf9023f1e17a359663e241
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2022-08-02 13:40:16 -0400

    Add United Way Use Case

---
+++ src/components/usecasestatement.jsx
@@ -0,0 +1,35 @@
+export const UseCaseStatement = () => {
+  return (
+    <div id="testimonials" style={{ paddingBottom: 0 }}>
+      <div className="section-title text-center">
+        <h2 style={{ marginTop: 90 }}>United Way Use Case</h2>
+      </div>
+      <div
+        style={{
+          textAlign: "center",
+          maxWidth: 970,
+          margin: "auto",
+          padding: "20px 20px 0px 20px"
+        }}
+      >
+        <p style={{ fontSize: 22, fontWeight: 500 }}>
+          We’ve been working with United Way Waterloo Region Since March 2020,
+          helping them improve their granting process. They currently use our
+          platform for their year-round granting streams that total upwards of
+          $4 million a year.
+        </p>
+        <p style={{ fontSize: 22, fontWeight: 500 }}>
+          Through close collaboration, we’ve improved the application collection
+          and review process for 1000s of applications and reviews.
+        </p>
+        <p style={{ fontSize: 22, fontWeight: 500 }}>
+          The Senior Community Investment Manager at United Way Waterloo Region,
+          Brian, said that one of the best parts of working with DecisionHub is
+          “being able to get ahold of [them] in a timely fashion” as DecisionHub
+          is a small team, and “one of the advantages of working with a smaller
+          developer is the connection, and the time it takes to hear back”.
+        </p>
+      </div>
+    </div>
+  );
+};
