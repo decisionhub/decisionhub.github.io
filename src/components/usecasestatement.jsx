e81499c193d91f801e7017e9f3153d67b3801de5
Ahmed Hamodi <ahmed.r.hamodi@gmail.com>
2022-08-03 12:01:12 -0400

    Update use case + remove autoplay (#105)

--- src/components/usecasestatement.jsx
+++ src/components/usecasestatement.jsx
@@ -12,22 +12,29 @@ export const UseCaseStatement = () => {
           padding: "20px 20px 0px 20px"
         }}
       >
-        <p style={{ fontSize: 22, fontWeight: 500 }}>
+        <p style={{ fontSize: 22 }}>
           We’ve been working with United Way Waterloo Region Since March 2020,
           helping them improve their granting process. They currently use our
           platform for their year-round granting streams that total upwards of
           $4 million a year.
         </p>
-        <p style={{ fontSize: 22, fontWeight: 500 }}>
+        <p style={{ fontSize: 22 }}>
           Through close collaboration, we’ve improved the application collection
           and review process for 1000s of applications and reviews.
         </p>
-        <p style={{ fontSize: 22, fontWeight: 500 }}>
-          The Senior Community Investment Manager at United Way Waterloo Region,
-          Brian, said that one of the best parts of working with DecisionHub is
-          “being able to get ahold of [them] in a timely fashion” as DecisionHub
-          is a small team, and “one of the advantages of working with a smaller
-          developer is the connection, and the time it takes to hear back”.
+        <p style={{ fontSize: 22 }}>
+          The <em>Senior Community Investment Manager</em> at{" "}
+          <em>United Way Waterloo Region</em>, Brian, said that one of the best
+          parts of working with DecisionHub is{" "}
+          <span style={{ fontWeight: 600 }}>
+            “being able to get ahold of [them] in a timely fashion”
+          </span>{" "}
+          as DecisionHub is a small team, and{" "}
+          <span style={{ fontWeight: 600 }}>
+            “one of the advantages of working with a smaller developer is the
+            connection, and the time it takes to hear back”
+          </span>
+          .
         </p>
       </div>
     </div>
